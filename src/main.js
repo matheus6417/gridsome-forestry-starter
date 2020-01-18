ed0f910f64772f47c8f39d747b3981b0e4779424
Matheus Cezar <matheus6417@gmail.com>
2020-01-18 14:49:09 -0300

    layouts css

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import settings from "../data/theme.json";
 import Vue from 'vue';
 import 'swiper/dist/css/swiper.css'
 import VueTabs from 'vue-nav-tabs'
-import 'vue-nav-tabs/themes/vue-tabs.css'
 Vue.use(VueTabs)
 
 export default function(Vue, { head }) {
--- src/pages/Solutions.vue
+++ src/pages/Solutions.vue
@@ -12,7 +12,7 @@
             <h3>We create digital solutions to make your business fit for the future</h3>
           </div>
         </div>
-        <div class="container">
+        <div buttons class="container">
           <div class="row">
             <div class="solutions-index__header-wrapper">
               <div class="solutions-index__header">
@@ -20,36 +20,54 @@
                   class="solutions-index__filter-buttons solutions-index__filter-buttons-smooth-scroll"
                 >
                   <div class="blog-tag solutions-index">
-                    <button class="filter-button selected blog-tag solutions-index">
+                    <button
+                      @click="solutionpage = 'docs'"
+                      class="filter-button blog-tag solutions-index"
+                      :class="{'selected':solutionpage === 'docs'}"
+                    >
                       <span class="placeholder-text">AI Document Management</span>
                       <div class="filter-button__text">AI Document Management</div>
                     </button>
                   </div>
                   <div class="blog-tag solutions-index">
-                    <button class="filter-button blog-tag solutions-index">
+                    <button
+                      @click="solutionpage = 'support'"
+                      class="filter-button blog-tag solutions-index"
+                      :class="{'selected':solutionpage === 'support'}"
+                    >
                       <span class="placeholder-text">AI Technical Support Chatbot</span>
                       <div class="filter-button__text">AI Technical Support Chatbot</div>
                     </button>
                   </div>
                   <div class="blog-tag solutions-index">
-                    <button class="filter-button blog-tag solutions-index">
+                    <button
+                      @click="solutionpage = 'facial'"
+                      class="filter-button blog-tag solutions-index"
+                      :class="{'selected':solutionpage === 'facial'}"
+                    >
                       <span class="placeholder-text">Facial Recognition</span>
                       <div class="filter-button__text">Facial Recognition</div>
                     </button>
                   </div>
                   <div class="blog-tag solutions-index">
-                    <button class="filter-button blog-tag solutions-index">
+                    <button
+                      @click="solutionpage = 'claims'"
+                      class="filter-button blog-tag solutions-index"
+                      :class="{'selected':solutionpage === 'claims'}"
+                    >
                       <span class="placeholder-text">AI Chatbot for HR Claims</span>
                       <div class="filter-button__text">AI Chatbot for HR Claims</div>
                     </button>
                   </div>
                 </div>
-                <h1 class="solutions-index__title">Firebox AI Document Management</h1>
-                <span class="solutions-index__description">
-                  The Firebox is a Smart Content Repository that is essentially
-                  a Document Management system powered by AI.
-                  Firebox uses AI to understand your photos and documents to
-                  help you better categorize, search and protect your files.
+                <span v-if="solutionpage === 'docs'">
+                  <h1 class="solutions-index__title">Firebox AI Document Management</h1>
+                  <span class="solutions-index__description">
+                    The Firebox is a Smart Content Repository that is essentially
+                    a Document Management system powered by AI.
+                    Firebox uses AI to understand your photos and documents to
+                    help you better categorize, search and protect your files.
+                  </span>
                 </span>
               </div>
             </div>
@@ -57,14 +75,24 @@
         </div>
 
         <div
+          v-if="solutionpage === 'docs'"
+          first-content
           class="solutions-index__solutions-card-wrapper"
           style="background-color: rgb(238, 238, 238);"
         >
           <div class="container">
             <div class="row">
-              <app-work-card class="solutions-index" _nghost-serverapp-c6>
+              <div class="solutions-index app-work-card">
                 <article class="solutions-card solutions-index">
-                  <div class="solutions-card__left col-lg-6 offset-lg-1 col-md-6">
+                  <div class="solutions-card__left col-lg-6 col-md-6">
+                    <img
+                      class="solutions-card__mockup"
+                      src="https://s3-eu-central-1.amazonaws.com/sc-website-2019-uploads-current/wp-content/uploads/2019/09/09114303/KodakMainImage-e1567607230746.png"
+                      alt="https://s3-eu-central-1.amazonaws.com/sc-website-2019-uploads-current/wp-content/uploads/2019/09/09114303/KodakMainImage-e1567607230746.png"
+                      style="transform: translateX(0px); opacity: 1;"
+                    />
+                  </div>
+                  <div class="solutions-card__right col-lg-6 col-md-6">
                     <h2
                       class="solutions-card__title main-title"
                       style="opacity: 1; transform: translateY(0px);"
@@ -89,124 +117,136 @@
                       model. It could be a hosting model that helps
                       eliminates the upfront investment cost.
                     </p>
-                    <div class>
-                      <app-read-more _nghost-serverapp-c9 class>
-                        <a class="read-more undefined" href="/work/details/kodak-moments">
-                          <div class="read-more__image">
-                            <img
-                              class="read-more__arrow"
-                              alt="arrow"
-                              src="assets/images/icon-arrow-transparent-right.svg"
-                              style
-                            />
-                            <div
-                              class="read-more__border"
-                              style="background-size: 100% 2px, 2px 100%, 100% 2px, 2px 100%;"
-                            ></div>
-                          </div>
-                          <div
-                            class="read-more__text"
-                            style="transform: translateX(0px); opacity: 1;"
-                          >
-                            <p class="read-more__line">read the</p>
-                            <p class="read-more__line">case study</p>
-                          </div>
-                        </a>
-                      </app-read-more>
-
-                      <app-read-more _nghost-serverapp-c9 class>
-                        <a class="read-more undefined" href="/work/details/kodak-moments">
-                          <div class="read-more__image">
-                            <img
-                              class="read-more__arrow"
-                              alt="arrow"
-                              src="assets/images/icon-arrow-transparent-right.svg"
-                              style
-                            />
-                            <div
-                              class="read-more__border"
-                              style="background-size: 100% 2px, 2px 100%, 100% 2px, 2px 100%;"
-                            ></div>
-                          </div>
-                          <div
-                            class="read-more__text"
-                            style="transform: translateX(0px); opacity: 1;"
-                          >
-                            <p class="read-more__line">read</p>
-                            <p class="read-more__line">more</p>
-                          </div>
-                        </a>
-                      </app-read-more>
-                    </div>
                   </div>
-                  <div class="solutions-card__right">
-                    <img
-                      class="solutions-card__mockup-mobile"
-                      src="https://s3-eu-central-1.amazonaws.com/sc-website-2019-uploads-current/wp-content/uploads/2019/09/09114303/KodakMainImage-e1567607230746.png"
-                      alt="https://s3-eu-central-1.amazonaws.com/sc-website-2019-uploads-current/wp-content/uploads/2019/09/09114303/KodakMainImage-e1567607230746.png"
-                      style="transform: translateX(0px); opacity: 1;"
-                    />
-                    <div class="solutions-card__mockup">
-                      <img
-                        class="solutions-card__img-main"
-                        src="https://s3-eu-central-1.amazonaws.com/sc-website-2019-uploads-current/wp-content/uploads/2019/09/04085532/kodak.png"
-                        alt="Kodak Moments Supercharge"
-                        style="transform: translateX(0px); opacity: 1;"
-                      />
-
-                      <img
-                        class="solutions-card__image-layer"
-                        src="https://s3-eu-central-1.amazonaws.com/sc-website-2019-uploads-current/wp-content/uploads/2019/09/09114255/KodakFilmSmall02.png"
-                        alt
-                        style
-                      />
-                      <img
-                        class="solutions-card__image-layer"
-                        src="https://s3-eu-central-1.amazonaws.com/sc-website-2019-uploads-current/wp-content/uploads/2019/09/09114255/KodakFilmSmall01.png"
-                        alt
-                        style
-                      />
-                    </div>
-                  </div>
-                  <img
-                    class="solutions-card__arrow"
-                    src="assets/images/icon-header-arrow.svg"
-                    style="opacity: 0; transform: translateY(-2rem);"
-                  />
                 </article>
-              </app-work-card>
+              </div>
             </div>
           </div>
         </div>
+        <div v-if="solutionpage === 'docs'" features-title class="row">
+          <h2 class="solutions-index__footer-title main-title">Firebox AI Features</h2>
+        </div>
+        <div v-if="solutionpage === 'docs'" features-content class="container other-projects">
+          <vue-tabs direction="vertical" type="pills">
+            <v-tab>
+              <span slot="title">
+                <span class="tab-title">Files Management</span>
+                <span class="tab-subtitle">
+                  Allows users to upload and store all types of documents, images and videos
+                  The Repository's structure is similar to the windows folder design where users can create folders and many levels of subfolders under it.
+                </span>
+              </span>
 
-        <div class="container other-projects">
-          <div class="row">
-            <div class="col-lg-6 offset-lg-1">
-              <h2 class="solutions-index__footer-title main-title">Firebox AI Features</h2>
-            </div>
-          </div>
-          <vue-tabs>
-            <v-tab title="First tab">First tab content</v-tab>
+              <div class="tab-content-img-wrapper">
+                <img
+                  class="solutions-card__features-img"
+                  src="https://s3-eu-central-1.amazonaws.com/sc-website-2019-uploads-current/wp-content/uploads/2019/09/09114303/KodakMainImage-e1567607230746.png"
+                  alt="https://s3-eu-central-1.amazonaws.com/sc-website-2019-uploads-current/wp-content/uploads/2019/09/09114303/KodakMainImage-e1567607230746.png"
+                  style="transform: translateX(0px); opacity: 1;"
+                />
+              </div>
+            </v-tab>
 
-            <v-tab title="Second tab">Second tab content</v-tab>
+            <v-tab>
+              <span slot="title">
+                <span class="tab-title">Automagically categorization</span>
+                <span class="tab-subtitle">
+                  Files document into the appropriate folder in the Repository intelligently.
+                  Analyses the contents inside your file and automatically organizes and tags them for retrieval purposes
+                </span>
+              </span>
+              <div class="tab-content-img-wrapper">
+                <img
+                  class="solutions-card__features-img"
+                  src="https://s3-eu-central-1.amazonaws.com/sc-website-2019-uploads-current/wp-content/uploads/2019/09/09114303/KodakMainImage-e1567607230746.png"
+                  alt="https://s3-eu-central-1.amazonaws.com/sc-website-2019-uploads-current/wp-content/uploads/2019/09/09114303/KodakMainImage-e1567607230746.png"
+                  style="transform: translateX(0px); opacity: 1;"
+                />
+              </div>
+            </v-tab>
 
-            <v-tab title="Third tab">Third tab content</v-tab>
-          </vue-tabs>Files Management
-          • Allows users to upload and store all types of documents, images and videos
-          Automagically categorization
-          • Files document into the appropriate folder in the Repository intelligently.
-          • Analyses the contents inside your file and automatically organizes and tags them for retrieval purposes
-          Documents Content Extration
-          • Extracts text from uploaded image and embedded images inside Microsoft Doc and PDF.
-          Video Content Extraction
-          • Extracts text frame by frame from the uploaded video file. All objects in the video are searchable
-          Version Control
-          • Supports version control. Stores only the differential parts of the changes each time, thereby reduces storage requirements.
-          Built-in Reader
-          • Has a built-in reader to read all kinds of document in its Repository. Can also read all Microsoft document types.
-          AI-Powered Security
-          • Has robust security features to prevent unauthorize access of the document. All users are assigned access rights based on their roles and organization hierarchy.
-        </div>The Repository's structure is similar to the windows folder design where users can create folders and many levels of subfolders under it.
+            <v-tab>
+              <span slot="title">
+                <span class="tab-title">Documents Content Extration</span>
+                <span
+                  class="tab-subtitle"
+                >Extracts text from uploaded image and embedded images inside Microsoft Doc and PDF.</span>
+              </span>
+              <div class="tab-content-img-wrapper">
+                <img
+                  class="solutions-card__features-img"
+                  src="https://s3-eu-central-1.amazonaws.com/sc-website-2019-uploads-current/wp-content/uploads/2019/09/09114303/KodakMainImage-e1567607230746.png"
+                  alt="https://s3-eu-central-1.amazonaws.com/sc-website-2019-uploads-current/wp-content/uploads/2019/09/09114303/KodakMainImage-e1567607230746.png"
+                  style="transform: translateX(0px); opacity: 1;"
+                />
+              </div>
+            </v-tab>
+            <v-tab>
+              <span slot="title">
+                <span class="tab-title">Video Content Extraction</span>
+                <span
+                  class="tab-subtitle"
+                >Extracts text frame by frame from the uploaded video file. All objects in the video are searchable</span>
+              </span>
+              <div class="tab-content-img-wrapper">
+                <img
+                  class="solutions-card__features-img"
+                  src="https://s3-eu-central-1.amazonaws.com/sc-website-2019-uploads-current/wp-content/uploads/2019/09/09114303/KodakMainImage-e1567607230746.png"
+                  alt="https://s3-eu-central-1.amazonaws.com/sc-website-2019-uploads-current/wp-content/uploads/2019/09/09114303/KodakMainImage-e1567607230746.png"
+                  style="transform: translateX(0px); opacity: 1;"
+                />
+              </div>
+            </v-tab>
+            <v-tab>
+              <span slot="title">
+                <span class="tab-title">Version Control</span>
+                <span
+                  class="tab-subtitle"
+                >Supports version control. Stores only the differential parts of the changes each time, thereby reduces storage requirements.</span>
+              </span>
+              <div class="tab-content-img-wrapper">
+                <img
+                  class="solutions-card__features-img"
+                  src="https://s3-eu-central-1.amazonaws.com/sc-website-2019-uploads-current/wp-content/uploads/2019/09/09114303/KodakMainImage-e1567607230746.png"
+                  alt="https://s3-eu-central-1.amazonaws.com/sc-website-2019-uploads-current/wp-content/uploads/2019/09/09114303/KodakMainImage-e1567607230746.png"
+                  style="transform: translateX(0px); opacity: 1;"
+                />
+              </div>
+            </v-tab>
+            <v-tab>
+              <span slot="title">
+                <span class="tab-title">Built-in Reader</span>
+                <span
+                  class="tab-subtitle"
+                >Has a built-in reader to read all kinds of document in its Repository. Can also read all Microsoft document types.</span>
+              </span>
+              <div class="tab-content-img-wrapper">
+                <img
+                  class="solutions-card__features-img"
+                  src="https://s3-eu-central-1.amazonaws.com/sc-website-2019-uploads-current/wp-content/uploads/2019/09/09114303/KodakMainImage-e1567607230746.png"
+                  alt="https://s3-eu-central-1.amazonaws.com/sc-website-2019-uploads-current/wp-content/uploads/2019/09/09114303/KodakMainImage-e1567607230746.png"
+                  style="transform: translateX(0px); opacity: 1;"
+                />
+              </div>
+            </v-tab>
+            <v-tab>
+              <span slot="title">
+                <span class="tab-title">AI-Powered Security</span>
+                <span
+                  class="tab-subtitle"
+                >Has robust security features to prevent unauthorize access of the document. All users are assigned access rights based on their roles and organization hierarchy.</span>
+              </span>
+              <div class="tab-content-img-wrapper">
+                <img
+                  class="solutions-card__features-img"
+                  src="https://s3-eu-central-1.amazonaws.com/sc-website-2019-uploads-current/wp-content/uploads/2019/09/09114303/KodakMainImage-e1567607230746.png"
+                  alt="https://s3-eu-central-1.amazonaws.com/sc-website-2019-uploads-current/wp-content/uploads/2019/09/09114303/KodakMainImage-e1567607230746.png"
+                  style="transform: translateX(0px); opacity: 1;"
+                />
+              </div>
+            </v-tab>
+          </vue-tabs>
+        </div>
       </div>
     </span>
     <Bottom />
@@ -222,11 +262,14 @@ export default {
   },
   data() {
     return {
-      settings: require("../../data/solutions.json")
+      settings: require("../../data/solutions.json"),
+      solutionpage: "docs" // support | facial | claims
     };
   }
 };
 </script>
 <style lang="scss" scoped>
+@import "../styles/layout.css";
 @import "../styles/solutions.css";
+@import "../styles/tabs.css";
 </style>
