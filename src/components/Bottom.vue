2234eb9eac399cf0308c59d20acd67df254ca0ba
Matheus Cezar <matheus6417@gmail.com>
2020-01-17 21:18:35 -0300

    prep solutions

---
+++ src/components/Bottom.vue
@@ -0,0 +1,51 @@
+<template>
+  <article class="footer">
+    <!-- <div class="footer__social-media">
+      <a
+        class="footer__icon-link"
+        href="https://medium.com/supercharges-mobile-product-guide"
+        target="_blank"
+      >
+        <img alt="medium" class="footer__icon" src="assets/images/medium-icon.svg" />
+      </a>
+      <a class="footer__icon-link" href="https://www.linkedin.com/company/2147004" target="_blank">
+        <img alt="linkedin" class="footer__icon" src="assets/images/linkedin-icon.svg" />
+      </a>
+      <a class="footer__icon-link" href="https://www.facebook.com/TeamSupercharge" target="_blank">
+        <img alt="facebook" class="footer__icon" src="assets/images/icon-facebook.svg" />
+      </a>
+      <a class="footer__icon-link" href="https://twitter.com/TeamSupercharge" target="_blank">
+        <img alt="twitter" class="footer__icon" src="assets/images/twitter-icon.svg" />
+      </a>
+      <a class="footer__icon-link" href="https://www.instagram.com/supercharge.io" target="_blank">
+        <img alt="instagram" class="footer__icon" src="assets/images/instagram-icon.svg" />
+      </a>
+      <a class="footer__icon-link" href="https://github.com/team-supercharge" target="_blank">
+        <img alt="github" class="footer__icon" src="assets/images/github-icon.svg" />
+      </a>
+    </div>-->
+    <div class="footer__copyright">
+      Copyright 2020 -
+      <strong>Regulus DT</strong> - all rights reserved
+    </div>
+    <div class="footer__policies">
+      <a class="footer__policy" routerlink="/policy/cookie-policy" href="/">Home</a>
+      <a class="footer__policy" routerlink="/policy/privacy-policy" href="/about">Contact</a>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      settings: require("../../data/theme.json")
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+@import "../styles/bottom.css";
+</style>
+  
--- src/pages/Solutions.vue
+++ src/pages/Solutions.vue
@@ -1,11 +1,241 @@
 <template>
   <Layout>
-    <span>solutions</span>
+    <span>
+      <div class="study-index">
+        <div class="study-index__header-image-wrapper">
+          <img
+            class="study-index__header-image ng-star-inserted"
+            src="https://s3-eu-central-1.amazonaws.com/sc-website-2019-uploads-current/wp-content/uploads/2019/09/09114240/featured_illustration_all3D01_1920x539.png"
+          />
+          <div class="about__title-flex row">
+            <h2 class="main-title">AI-Powered Solutions to Your Business</h2>
+            <h3>We create digital solutions to make your fit for the future</h3>
+          </div>
+          <div class="study-index__white-box"></div>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="study-index__header-wrapper">
+              <div class="study-index__header">
+                <div class="study-index__filter-buttons study-index__filter-buttons-smooth-scroll">
+                  <app-filter-button class="blog-tag study-index ng-star-inserted">
+                    <a
+                      class="filter-button selected blog-tag study-index ng-star-inserted"
+                      href="/work"
+                    >
+                      <span class="placeholder-text ng-star-inserted">AI Document Management</span>
+                      <div class="filter-button__text ng-star-inserted">AI Document Management</div>
+                    </a>
+                  </app-filter-button>
+                  <app-filter-button class="blog-tag study-index ng-star-inserted">
+                    <a
+                      class="filter-button blog-tag study-index ng-star-inserted"
+                      href="/work/financial-services"
+                    >
+                      <span class="placeholder-text ng-star-inserted">AI Technical Support Chatbot</span>
+                      <div class="filter-button__text ng-star-inserted">AI Technical Support Chatbot</div>
+                    </a>
+                  </app-filter-button>
+                  <app-filter-button class="blog-tag study-index ng-star-inserted">
+                    <a
+                      class="filter-button blog-tag study-index ng-star-inserted"
+                      href="/work/healthcare"
+                    >
+                      <span class="placeholder-text ng-star-inserted">Facial Recognition</span>
+                      <div class="filter-button__text ng-star-inserted">Facial Recognition</div>
+                    </a>
+                  </app-filter-button>
+                  <app-filter-button class="blog-tag study-index ng-star-inserted">
+                    <a
+                      class="filter-button blog-tag study-index ng-star-inserted"
+                      href="/work/e-commerce"
+                    >
+                      <span class="placeholder-text ng-star-inserted">AI Chatbot for HR Claims</span>
+                      <div class="filter-button__text ng-star-inserted">AI Chatbot for HR Claims</div>
+                    </a>
+                  </app-filter-button>
+                </div>
+                <h1 class="study-index__title">Firebox AI Document Management</h1>
+                <span class="study-index__description">
+                  The Firebox is a Smart Content Repository that is essentially
+                  a Document Management system powered by AI.
+                  Firebox uses AI to understand your photos and documents to
+                  help you better categorize, search and protect your files.
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div
+          class="study-index__study-card-wrapper ng-star-inserted"
+          style="background-color: rgb(238, 238, 238);"
+        >
+          <div class="container">
+            <div class="row">
+              <app-work-card class="study-index ng-tns-c6-31" _nghost-serverapp-c6>
+                <article class="ng-tns-c6-31 study-card study-index">
+                  <div class="ng-tns-c6-31 study-card__left col-lg-4 offset-lg-1 col-md-6">
+                    <h2
+                      class="study-card__title main-title ng-trigger ng-trigger-titleAppear"
+                      style="opacity: 1; transform: translateY(0px);"
+                    >
+                      The Document Management
+                      System (DMS)
+                    </h2>
+                    <p
+                      class="study-card__description ng-trigger ng-trigger-paragraphAppear"
+                      style="opacity: 1; transform: translateY(0px);"
+                    >
+                      DMS is often perceived as being expensive and
+                      complicated to use. While most organizations
+                      understand the necessity and importance of such
+                      a system, it is the price and total-cost-of-ownership
+                      (TCO) that is difficult for them to justify.
+                      That was in the past. Today, thanks to the
+                      advancement in AI technology, DMS has
+                      become more powerful and yet affordable.
+                      Most importantly, AI-powered DMS may not
+                      need to be a one time-off-cost for an on-prem
+                      model. It could be a hosting model that helps
+                      eliminates the upfront investment cost.
+                    </p>
+                    <div class="ng-tns-c6-31">
+                      <app-read-more
+                        _nghost-serverapp-c9
+                        class="ng-tns-c9-34 ng-tns-c6-31 ng-star-inserted"
+                      >
+                        <a
+                          class="ng-tns-c9-34 read-more undefined ng-star-inserted"
+                          href="/work/details/kodak-moments"
+                        >
+                          <div class="read-more__image ng-tns-c9-34 ng-star-inserted">
+                            <img
+                              class="read-more__arrow ng-trigger ng-trigger-enlarge"
+                              alt="arrow"
+                              src="assets/images/icon-arrow-transparent-right.svg"
+                              style
+                            />
+                            <div
+                              class="read-more__border ng-trigger ng-trigger-drawBorder"
+                              style="background-size: 100% 2px, 2px 100%, 100% 2px, 2px 100%;"
+                            ></div>
+                          </div>
+                          <div
+                            class="read-more__text ng-trigger ng-trigger-textAnimation ng-star-inserted"
+                            style="transform: translateX(0px); opacity: 1;"
+                          >
+                            <p class="read-more__line">read the</p>
+                            <p class="read-more__line">case study</p>
+                          </div>
+                        </a>
+                      </app-read-more>
+
+                      <app-read-more
+                        _nghost-serverapp-c9
+                        class="ng-tns-c9-35 ng-tns-c6-31 ng-star-inserted"
+                      >
+                        <a
+                          class="ng-tns-c9-35 read-more undefined ng-star-inserted"
+                          href="/work/details/kodak-moments"
+                        >
+                          <div class="read-more__image ng-tns-c9-35 ng-star-inserted">
+                            <img
+                              class="read-more__arrow ng-trigger ng-trigger-enlarge"
+                              alt="arrow"
+                              src="assets/images/icon-arrow-transparent-right.svg"
+                              style
+                            />
+                            <div
+                              class="read-more__border ng-trigger ng-trigger-drawBorder"
+                              style="background-size: 100% 2px, 2px 100%, 100% 2px, 2px 100%;"
+                            ></div>
+                          </div>
+                          <div
+                            class="read-more__text ng-trigger ng-trigger-textAnimation ng-star-inserted"
+                            style="transform: translateX(0px); opacity: 1;"
+                          >
+                            <p class="read-more__line">read</p>
+                            <p class="read-more__line">more</p>
+                          </div>
+                        </a>
+                      </app-read-more>
+                    </div>
+                  </div>
+                  <div class="study-card__right">
+                    <img
+                      class="study-card__mockup-mobile ng-trigger ng-trigger-mainImageFadeOut"
+                      src="https://s3-eu-central-1.amazonaws.com/sc-website-2019-uploads-current/wp-content/uploads/2019/09/09114303/KodakMainImage-e1567607230746.png"
+                      alt="https://s3-eu-central-1.amazonaws.com/sc-website-2019-uploads-current/wp-content/uploads/2019/09/09114303/KodakMainImage-e1567607230746.png"
+                      style="transform: translateX(0px); opacity: 1;"
+                    />
+                    <div class="study-card__mockup ng-trigger ng-trigger-listAnimation">
+                      <img
+                        class="study-card__img-main ng-trigger ng-trigger-mainImageAppear ng-trigger-mainImageFadeOut"
+                        src="https://s3-eu-central-1.amazonaws.com/sc-website-2019-uploads-current/wp-content/uploads/2019/09/04085532/kodak.png"
+                        alt="Kodak Moments Supercharge"
+                        style="transform: translateX(0px); opacity: 1;"
+                      />
+
+                      <img
+                        class="study-card__image-layer ng-trigger ng-trigger-triangle ng-tns-c6-31 ng-star-inserted ng-animating"
+                        src="https://s3-eu-central-1.amazonaws.com/sc-website-2019-uploads-current/wp-content/uploads/2019/09/09114255/KodakFilmSmall02.png"
+                        alt
+                        style
+                      />
+                      <img
+                        class="study-card__image-layer ng-trigger ng-trigger-triangle ng-tns-c6-31 ng-star-inserted ng-animating"
+                        src="https://s3-eu-central-1.amazonaws.com/sc-website-2019-uploads-current/wp-content/uploads/2019/09/09114255/KodakFilmSmall01.png"
+                        alt
+                        style
+                      />
+                    </div>
+                  </div>
+                  <img
+                    class="study-card__arrow ng-trigger ng-trigger-arrowAppear"
+                    src="assets/images/icon-header-arrow.svg"
+                    style="opacity: 0; transform: translateY(-2rem);"
+                  />
+                </article>
+              </app-work-card>
+            </div>
+          </div>
+        </div>
+
+        <div class="container other-projects">
+          <div class="row">
+            <div class="col-lg-6 offset-lg-1">
+              <h2 class="study-index__footer-title main-title">Firebox AI Features</h2>
+            </div>
+          </div>Files Management
+          • Allows users to upload and store all types of documents, images and videos
+          Automagically categorization
+          • Files document into the appropriate folder in the Repository intelligently.
+          • Analyses the contents inside your file and automatically organizes and tags them for retrieval purposes
+          Documents Content Extration
+          • Extracts text from uploaded image and embedded images inside Microsoft Doc and PDF.
+          Video Content Extraction
+          • Extracts text frame by frame from the uploaded video file. All objects in the video are searchable
+          Version Control
+          • Supports version control. Stores only the differential parts of the changes each time, thereby reduces storage requirements.
+          Built-in Reader
+          • Has a built-in reader to read all kinds of document in its Repository. Can also read all Microsoft document types.
+          AI-Powered Security
+          • Has robust security features to prevent unauthorize access of the document. All users are assigned access rights based on their roles and organization hierarchy.
+        </div>The Repository's structure is similar to the windows folder design where users can create folders and many levels of subfolders under it.
+      </div>
+    </span>
+    <Bottom />
   </Layout>
 </template>
 
 <script>
+import Bottom from "@/components/Bottom";
+
 export default {
+  components: {
+    Bottom
+  },
   data() {
     return {
       settings: require("../../data/solutions.json")
@@ -13,3 +243,6 @@ export default {
   }
 };
 </script>
+<style lang="scss">
+@import "../styles/solutions.css";
+</style>
